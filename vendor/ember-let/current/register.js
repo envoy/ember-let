8acbd65a532ed43909930abdab313513dfcd6c8e
Esteban Arango Medina <marranoparael31@gmail.com>
2018-05-09 14:40:24 -0500

    Import private _registerMacros directly from `ember-glimmer`

--- vendor/ember-let/current/register.js
+++ vendor/ember-let/current/register.js
@@ -1,8 +1,8 @@
 // Support for 2.15+
-const { registerMacros } = Ember.__loader.require('ember-glimmer/syntax');
+const { _registerMacros } = Ember.__loader.require('ember-glimmer');
 const { compileList } = Ember.__loader.require('@glimmer/runtime');
 
-registerMacros(blocks => {
+_registerMacros(blocks => {
   blocks.add('let', (params, hash, _default, inverse, builder) => {
     compileList(params, builder);
     builder.invokeStatic(_default, params.length);
--- vendor/ember-let/ember-lt-2-15/register.js
+++ vendor/ember-let/ember-lt-2-15/register.js
@@ -1,8 +1,8 @@
 // Support for 2.13 - 2.14
-const { registerMacros } = Ember.__loader.require('ember-glimmer/syntax');
+const { _registerMacros } = Ember.__loader.require('ember-glimmer/syntax');
 const { compileArgs } = Ember.__loader.require('@glimmer/runtime');
 
-registerMacros(blocks => {
+_registerMacros(blocks => {
   blocks.add('let', (sexp, builder) => {
     let [,, params, hash, _default] = sexp;
     let args = compileArgs(params, hash, builder);
